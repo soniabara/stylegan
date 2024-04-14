5d52cd4ddd6772790412c25f5f5782a872e21c15
soniabara <119227584+soniabara@users.noreply.github.com>
2024-04-14 13:07:44 +0530

    Update tfutil.py

--- dnnlib/tflib/tfutil.py
+++ dnnlib/tflib/tfutil.py
@@ -31,7 +31,10 @@ def is_tf_expression(x: Any) -> bool:
     return isinstance(x, (tf.Tensor, tf.Variable, tf.Operation))
 
 
-def shape_to_list(shape: Iterable[tf.Dimension]) -> List[Union[int, None]]:
+from typing import Iterable, List, Union
+
+def shape_to_list(shape: Iterable) -> List[Union[int, None]]:
+
     """Convert a Tensorflow shape to a list of ints."""
     return [dim.value for dim in shape]
 
